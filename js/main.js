d6df3a4af21b37ed9e611cba6b06aa8b4e3b5ffd
thesmertnik <eyeofwhitenight@yandex.ru>
2023-07-19 13:29:47 +0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', loadBG);
+
+// Проверить наличие данных
+if (localStorage.getItem('data')) {
+  // Использовать кешированные данные
+  const data = JSON.parse(localStorage.getItem('data'));
+
+} else {
+  // Запросить данные с сервера
+  fetch('/data')
+    .then(response => response.json())
+    .then(data => {
+      // Сохранить данные в хранилище
+      localStorage.setItem('data', JSON.stringify(data));
+
+      // Использовать данные
+    })
+}
